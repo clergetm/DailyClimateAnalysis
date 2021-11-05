ab2ebaf2b2769aaf80e1c4f796d3fa01694ddca2
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2021-11-04 21:38:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import os
 import numpy as np
-
-from Utils.Get import get_data, get_nan, get_duplicate
+import pandas as pd
+from Utils.Get import get_data, get_nan, get_duplicate, get_class
 
 if __name__ == '__main__':
-	file_path = "Files/DailyDelhiClimateTrain.csv"
-	if not (os.path.isfile(file_path)):
-		print("fichier manquant")
+	file_path_Train = "Files/DailyDelhiClimateTrain.csv"
+	file_path_Test = "Files/DailyDelhiClimateTest.csv"
+	if not (os.path.isfile(file_path_Train) and os.path.isfile(file_path_Test)):
+		print("fichiers manquants")
 		exit()
 		
-	# Variables
-	dataFrame = get_data(file_path, sep=',', txt=True)
+	# Creation and cleaning of variables
+	dataFrame_Train = get_data(file_path_Train, sep=',', txt=True)
+	dataFrame_Test = get_data(file_path_Test, sep=',', txt=True)
 	
-	# Get information about duplicates and nan in the dataFrame
-	# print("Nan")
-	# print(get_nan(dataFrame))
-	# print("duplicates")
-	# print(get_duplicate(dataFrame))
-	# There is no duplicates and no nan in this dataset
+	# We concat all the data to have only one dataframe to use
+	dataFrame = pd.concat([dataFrame_Train, dataFrame_Test], axis=0).reset_index(drop=True)
+	del(dataFrame_Test, dataFrame_Train)
+	
+	print("Get information about duplicates and nan in the dataFrame")
+	print("Nan: ")
+	print(get_nan(dataFrame))
+	print("Duplicates: ")
+	print(get_duplicate(dataFrame, 'date'))
+	# There is one duplicate row :
+	# this is the last row of Train dataset and the first of Test dataset.
+	# Need to handle this
+	
+	# change dtype of date from 'object' to 'datetime'
+	dataFrame['date'] = pd.to_datetime(dataFrame['date'])
+
 	
